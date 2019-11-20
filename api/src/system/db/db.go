235efed0991ed2850e1d5a2a96f376c4037f9ae9
valeria@xanda.net <valeria@xanda.net>
2019-11-20 11:40:10 +0000

    Creating my user table

--- api/src/system/db/db.go
+++ api/src/system/db/db.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-xorm/xorm"
 )
 
-func Connect() (*xorm.Engine, error) {
-	return xorm.NewEngine("mysql", "test:test@tcp(localhost:3306)/test?charset-utf8")
+func Connect() (db *xorm.Engine, err error) {
+return = xorm.NewEngine("mysql", "test:test@tcp(localhost:3306)/test?charset-utf8")
+	
 }
